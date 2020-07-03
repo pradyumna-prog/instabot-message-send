eac1288db3157758ff15548882db3dba90593c6e
pradyumna-prog <pradyumnatripathi99@gmail.com>
2020-07-04 02:48:49 +0530

    first commit

---
+++ credentials.py
@@ -0,0 +1,3 @@
+username = 'pradyumnatripathi99@gmail.com'
+password = 'subhlabh@8'
+password2 = 'underwear@8'
---
+++ instabot.py
@@ -0,0 +1,44 @@
+from selenium import webdriver 
+from time import sleep
+from credentials import username, password
+
+def startInsta(driver):
+    driver.get("https://instagram.com/")
+    sleep(5)
+    #entering username
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input').send_keys(username)
+    #entering password
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input').send_keys(password)
+    #clicking login
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
+    sleep(5)
+    #clicking not now on turn on notification
+    driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
+
+def getFollowersList(driver):
+    #clicking on my profile
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/section/div[3]/div[1]/div/div[2]/div[1]/a').click()
+    sleep(1)
+    #clicking on followers
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a').click()
+def message(driver):
+    #search person
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input').send_keys('_kanishksrivastav_')
+    sleep(2)
+    #click on name
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]/div/div[2]/div/span').click()
+    sleep(3)
+    #click message box
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/button').click()
+    sleep(3)
+    #send msg
+    c = 30
+    while c:
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys('hiii\n')
+        sleep(1)
+        c-=1
+driver = webdriver.Chrome()
+sleep(1)
+startInsta(driver)
+sleep(5)
+message(driver)
