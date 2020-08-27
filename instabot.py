d32d545706a20defc835ee50f25f7c8ea9724165
pradyumna-prog <pradyumnatripathi99@gmail.com>
2020-08-27 19:21:39 +0530

    get unfollowers

--- instabot.py
+++ instabot.py
@@ -1,63 +1,91 @@
-from selenium import webdriver 
+from selenium import webdriver
 from time import sleep
-from credentials import username, password
+from credentials import password
 
-def startInsta(driver):
-    driver.get("https://instagram.com/")
+def startInsta(browser, username):
+    browser.get("https://instagram.com/")
     sleep(5)
     #entering username
-    driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(username)
+    browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(username)
     #entering password
     sleep(1)
-    driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(password)
+    browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(password)
     #clicking login
     sleep(1)
-    driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
+    browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
     #click on save info not now
     sleep(4)
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
     sleep(3)
     #clicking not now on turn on notification
-    driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+    browser.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
 
-def message(driver):
+def message(browser):
     #search person
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input').send_keys(input())
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input').send_keys(input())
     sleep(2)
     #click on name
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]/div/div[2]/div/span').click()
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]/div/div[2]/div/span').click()
     sleep(3)
     #click message box
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/button').click()
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/button').click()
     sleep(3)
     #send msg
     c = 30
     while c:
-        driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys('{}\n'.format(c))
+        browser.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea').send_keys('{}\n'.format(c))
         sleep(1)
         c-=1
 
-def getFollowersList(driver):
+def getList(browser, request):
+    li_index = 0
+    if request.lower() == "followers" : li_index = 2
+    elif request.lower() == "following" : li_index = 3
+    else:
+        print("Wrong Input")
+        return None
     #clicking on my profile picture
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/span/img').click()
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/span/img').click()
     sleep(1)
     #clicking on profile
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div/div[2]/div[2]/a[1]/div').click()
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div/div[2]/div[2]/a[1]/div').click()
     #clicking on followers
+    sleep(4)
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li['+str(li_index)+']/a').click()
+    #slecting the box containing lis with followers
     sleep(3)
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a').click()
-    sleep(3)
-    #driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-    sleep(12)
-    ul = driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul')
-    followers_li = ul.find_elements_by_tag_name("li")
+    followers_box = browser.find_element_by_xpath('/html/body/div[4]/div/div/div[2]')
+    #scorlling the box to last
+    pheight, height = 0, 1
+    while(pheight != height):
+        pheight = height
+        height = browser.execute_script("""
+            arguments[0].scrollTo(0, arguments[0].scrollHeight);
+            return arguments[0].scrollHeight;
+            """, followers_box)
+        sleep(5)
+    #fetching name
+    followers_links = followers_box.find_elements_by_tag_name("a")
     followers = []
-    for f in followers_li:
-        followers.append(f.text.split('\n')[1])
+    for f in followers_links:
+        followers.append(f.text)
+    #closing the followers box
+    browser.find_element_by_xpath('/html/body/div[4]/div/div/div[1]/div/div[2]/button').click()
+    #clicking Instagram
+    sleep(2)
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[1]/a/div/div/img').click()
+
     return followers
-driver = webdriver.Chrome()
-driver.maximize_window()
+
+browser = webdriver.Chrome()
+browser.maximize_window()
 sleep(1)
-startInsta(driver)
+startInsta(browser, 'pd_pradyumna')
 sleep(5)
-getFollowersList(driver)
+followers = getList(browser, "Followers")
+following = getList(browser, "Following")
+unfollowers = list(set(following) - set(followers))
+sleep(2)
+
+# s = "".join(unfollowers)
+# browser.get("localhost/instabot/index.php?list="+s)
